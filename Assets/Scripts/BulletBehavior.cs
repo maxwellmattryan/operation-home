e0ce007ee5b0ad0f38e1a6e8be993fdf36a2a1a9
Matthew <mumana98@gmail.com>
2019-01-26 14:15:32 -0600

    Add bullet prefab, bullet behavior script, shoot script, prefab folder,

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+
+        }
+        
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, player.transform.position, player.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float speed;
+    public GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.transform.Translate(Vector3.up * Time.deltaTime * speed);
+        
+    }
+}
