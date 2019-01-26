03617d5936820c2b91c38f9b44f23e954f112b51
BogieTheUnlucky <47048603+BogieTheUnlucky@users.noreply.github.com>
2019-01-26 15:49:16 -0600

    Warrior Behavior

---
+++ Assets/Scripts/WarriorBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorBehavior : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(this.gameObject);
+            //increment score
+        }
+        else if (other.gameObject.CompareTag("Earth"))
+        {
+            Destroy(this.gameObject);
+            //damage earth
+        }
+    }
+}
