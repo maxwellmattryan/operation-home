ceab31ff6aeef06ad2a6f8f967605143c17424ec
Matt <maxwellmattryan@gmail.com>
2019-01-26 15:10:17 -0600

    Add spawn controller script

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject Enemy;
+    public float spawnTimer;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTimer, 0);
+    }
+
+    void Spawn()
+    {
+        SetCoordinates();
+        Instantiate(Enemy);
+    }
+
+    void SetCoordinates()
+    {
+        Enemy.transform.RotateAround(Vector3.zero, Vector3.forward, Random.Range(0, 360));
+        Enemy.transform.LookAt(Vector3.zero);
+    }
+}
