315db7fe794c94ba129de86bc19a7c69a4061151
Matthew <mumana98@gmail.com>
2019-01-26 12:42:14 -0600

    Add playermovement

--- Assets/Scripts/CircularMovement.cs
+++ Assets/Scripts/CircularMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CircularMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Earth;
+    public float speed;
+   
     void Start()
     {
         
@@ -13,6 +15,15 @@ public class CircularMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if ((Input.GetKey(KeyCode.D))|| (Input.GetKey(KeyCode.RightArrow)))
+        {
+            transform.RotateAround(Earth.transform.position, Vector3.back, speed * Time.deltaTime);
+        }
+        if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
+        {
+            transform.RotateAround(Earth.transform.position, Vector3.forward, speed * Time.deltaTime);
+        }
+
     }
 }
