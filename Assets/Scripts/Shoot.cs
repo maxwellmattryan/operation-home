1b4c253cebfb1465b266efe43e3e7fd97e94828a
Matt <maxwellmattryan@gmail.com>
2019-01-26 20:29:00 -0600

    Removed FMOD from project

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,14 +7,9 @@ public class Shoot : MonoBehaviour
     public float speed;
     public GameObject bullet;
 
-    FMOD.Studio.EventInstance sound_shoot;
-    [FMODUnity.EventRef]
-    public string sound_shoot_path;
-
     void Start()
     {
         Destroy(gameObject, 1);
-        sound_shoot = FMODUnity.RuntimeManager.CreateInstance(sound_shoot_path);
     }
 
     void Update()
