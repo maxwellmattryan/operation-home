d70d3003e9cd16c4bfa393c08f7d7dba7b97eb52
Matthew <mumana98@gmail.com>
2019-01-27 11:56:20 -0600

    health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public float lifePoints = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifePoints == 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            lifePoints -= 1;
+
+
+        }
+
+    }
+
+}
